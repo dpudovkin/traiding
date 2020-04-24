ad52b43b6b14a35f31eb49d369bc6d6cf7fdbb43
Danil Pudovkin <pudovckin.d.v@gmail.com>
2020-04-24 15:07:09 +0500

    Modify main script and add params.json

--- analyze_bot.py
+++ analyze_bot.py
@@ -2,18 +2,61 @@
 
 __author__ = "Danil Pudovkin @dapudovkin"
 
+import csv
 import json
 import logging
 import argparse
 import yfinance as yf
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import matplotlib.ticker as mticker
 import mplfinance as mpf
 
-from datetime import datetime, timedelta
+from datetime import datetime as dt, timedelta
+from functools import reduce
 from sys import argv, exit
+from glob import glob
+
+# Get input params
+with open("./params.json", "r") as file:
+    params = json.load(file)
+
+
+origin_balance = params["balance"]
+broker_fee = params["broker_fee"]
+period = params["period"]
+tf_entry = params["tf_entry"]
+tf_exit = params["tf_exit"]
+short_ema = params["short_ema"]
+long_ema = params["long_ema"]
+signal_sma = params["signal_sma"]
+k_open = params["k_open"]
+k_close = params["k_close"]
+
+balance = origin_balance
+
+btcusd_30m_path = "./BTCUSDT/"
+ticker = "BTCUSD=X"
+price_type = "Close"
+end_date = dt.now()
+start_date = end_date - timedelta(days=period)
+
+orders = {
+    "order_type": [],
+    "open_time": [],
+    "open_price": [],
+    "close_time": [],
+    "close_price": [],
+    "net_income": []
+}
+
+equity = {
+    "balance": [],
+    "datetime": []
+}
+
 
 # Set logging parameters
 log_format = '%(asctime)s %(name)s: %(levelname)s: %(message)s'
@@ -25,126 +68,215 @@ logging.basicConfig(
 # Create logging instance
 logger = logging.getLogger(script_name)
 
-# Get input params
-with open("./params.json", "r") as file:
-    params = json.load(file)
 
-balance = params["balance"]
-broker_fee = params["broker_fee"]
-tf_entry = params["tf_entry"]
-tf_exit = params["tf_exit"]
-short_ema = params["short_ema"]
-long_ema = params["long_ema"]
-signal_sma = params["signal_sma"]
+def str_to_datetime(x):
+    return dt.strptime(x, "%Y-%m-%dT%H:%M:%S")
 
-ticker = "BTCUSD=X"
-price_type = "Close"
-end_date = datetime.now().date()
-start_date = end_date - timedelta(days=5)
+def to_datetime(x):
+    return dt.fromtimestamp(x.timestamp())
 
-deals = {
-    "order_type": [],
-    "open_time": [],
-    "open_price": [],
-    "close_time": [],
-    "close_price": [] 
-}
+def update_equity(balance, datetime):
+    equity["balance"].append(balance)
+    equity["datetime"].append(datetime)
 
-equity = []
-
-def update_equity(balance):
-    equity.append(balance)
-
-def get_equity():
-    print(equity)
+def get_fee(delta):
+    return abs(delta) * broker_fee
 
 def update_balance(delta):
     global balance
-    if delta < 0:
-        fee = abs(delta) * broker_fee
-    else:
-        fee = delta * broker_fee
+    balance += delta
 
-    balance += delta - fee
+def get_ticker(ticker, start, end, interval, source="url", path=None):
+    """
+    Params:
+        source: get ticer data from the specified source
+        path: path with csv files with the ticker data
+    """
 
-def get_balance():
-    print(balance)
+    logger.info(f"Get data for {ticker} with {interval} interval")
+    if source == "url":
+        df = yf.download(ticker, start, end, interval=interval).tz_localize(None)
+        df["Open"] = list(map(lambda x: np.round(x, 2), df["Open"]))
+        df["High"] = list(map(lambda x: np.round(x, 2), df["High"]))
+        df["Low"] = list(map(lambda x: np.round(x, 2), df["Low"]))
+        df["Close"] = list(map(lambda x: np.round(x, 2), df["Close"]))
+        return df
+    elif source == "files" and isinstance(path, str):
+        data = []
+        time = []
+        for file in sorted(glob(path + "*.csv")):
+            with open(file, "r") as f:
+                for row in csv.reader(f):
+                    row = row[0].split(";")
+                    if start < str_to_datetime(row[0]) < end:
+                        data.append(list(map(lambda x: float(x), row[1:])))
+                        time.append(str_to_datetime(row[0]))
+        df =  pd.DataFrame(data, index=time,
+            columns=["Open", "High", "Low", "Close", "Volume"]) 
+        df.index.name = "Datetime"
+        return df
 
-def get_ticker(ticker, start, end, interval):
-    return yf.download(ticker, start, end, interval=interval)
-
-def build_macd(td, tf):
+def build_macd(df, tf):
+    df["EMA(26)"] = pd.DataFrame.ewm(df[price_type], span=long_ema).mean()
+    df["EMA(12)"] = pd.DataFrame.ewm(df[price_type], span=short_ema).mean()
+    df["MACD"] = df["EMA(12)"] - df["EMA(26)"]
+    df["SMA(9)"] = df["MACD"].rolling(signal_sma).mean()
     logger.info(f"Build MACD indicator for {ticker} with {tf} interval.")
-    td["EMA(26)"] = pd.DataFrame.ewm(td[price_type], span=long_ema).mean()
-    td["EMA(12)"] = pd.DataFrame.ewm(td[price_type], span=short_ema).mean()
-    td["MACD"] = td["EMA(12)"] - td["EMA(26)"]
-    td["SMA(9)"] = td["MACD"].rolling(signal_sma).mean()
-
-    return td
+    return df
 
 def open_order(order_type, open_time, open_price):
-    logger.info(f"Open order with price {open_price} at {open_time}.")
-    deals["order_type"].append(order_type)
-    deals["open_time"].append(open_time)
-    deals["open_price"].append(open_price)
+    orders["order_type"].append(order_type)
+    orders["open_time"].append(open_time)
+    orders["open_price"].append(open_price)
     # Return order ID (index)
-    return len(deals["order_type"]) - 1
+    logger.info(f"Open order with price {open_price} at {open_time}.")
+    return len(orders["order_type"]) - 1
 
 def close_order(n, close_time, close_price):
-    logger.info(f"Close order with price {close_price} at {close_time}.")
-    deals["close_time"].append(close_time)
-    deals["close_price"].append(close_price)
+    orders["close_time"].append(close_time)
+    orders["close_price"].append(close_price)
 
-    if deals["order_type"][n] == "buy":
-        delta = deals["close_price"][n] - deals["open_price"][n]
-    elif deals["order_type"][n] == "sell":
-        delta = deals["open_price"][n] - deals["close_price"][n]
+    if orders["order_type"][n] == "buy":
+        delta = orders["close_price"][n] - orders["open_price"][n]
+    elif orders["order_type"][n] == "sell":
+        delta = orders["open_price"][n] - orders["close_price"][n]
 
-    update_balance(delta)
-    update_equity(balance)
+    net_income = delta - get_fee(delta)
+    orders["net_income"].append(net_income)
+    update_balance(net_income)
+    update_equity(balance, close_time)
+    logger.info(f"Close order with price {close_price} at {close_time}. "
+        f"Profit/Loss: {net_income} Balance: {balance}")
 
-update_equity(balance)
-    
-# Get ticker quotes 
-logger.info(f"Get data for {ticker} with {tf_entry} interval")
-td_entry = pd.DataFrame(get_ticker(ticker, start_date, end_date, tf_entry))
-logger.info(f"Get data for {ticker} with {tf_exit} interval")
-td_exit = pd.DataFrame(get_ticker(ticker, start_date, end_date, tf_exit))
+def open_cond1(df, i):
+    if df["MACD"][i] < 0:
+        return True
+    return False
 
-# Build MACD
-td_entry = build_macd(td_entry, tf_entry)
-td_exit = build_macd(td_exit, tf_exit)
+def open_cond2(df, i):
+    if (df["MACD"][i-1] < df["MACD"][i] > df["MACD"][i+1] and
+            df["MACD"][i] > 0):
+        return True
+    return False
 
-order_id = open_order("sell", "2020-04-20 11:00", 9000)
-close_order(order_id, "2020-04-20 12:00", 8000)
-get_balance()
-get_equity()
+def open_cond3(t1, t2):
+    if (t1["MACD"] >= t2["MACD"] and
+            t1["MACD"] > 0 and t2["MACD"] > 0):
+        return True
+    return False
 
-# mpf.plot(td)
-# plt.show()
+def open_cond4(t1, t2):
+    if (t1["Close"] > t1["Open"] and 
+            t1["Close"]*(1-k_open) <= t2["High"]):
+        return True
+    elif (t1["Close"] <= t1["Open"] and 
+            t1["Open"]*(1-k_open) <= t2["High"]): 
+        return True
+    return False
 
-# plt.plot(td.index, td[price_type])
-# plt.plot(td.index, td["EMA(26)"], color="yellow")
-# plt.plot(td.index, td["EMA(12)"], color="red")
-# plt.show()
+def close_cond1(df, i):
+    if (df["MACD"][i-1] > df["MACD"][i] < df["MACD"][i+1] and
+            df["MACD"][i-1] < 0 and df["MACD"][i] < 0):
+        return True
+    return False
 
-# plt.bar(td.index, td["MACD"])
-# plt.plot(td.index, td["SMA(9)"], color="red")
-# plt.show()
+def close_cond2(t1, t2):
+    if (t1["MACD"] <= t2["MACD"] and
+            t1["MACD"] < 0 and t2["MACD"] < 0):
+        return True
+    return False
 
-# logger.info(f"Analyze {ticker} for deals.")
-# td["Transaction"] = [None for i in range(len(td.index))]
+def close_cond3(t1, t2):
+    if (t1["Close"] > t1["Open"] and
+            t1["Open"]*(1+k_close) >= t2["Low"]):
+        return True
+    elif (t1["Close"] <= t1["Open"] and
+            t1["Close"]*(1+k_close) >= t2["Low"]): 
+        return True
+    return False
 
-# for i, time in enumerate(td.index):
-    
-#     if (td["SMA(9)"][i] > td["MACD"][i] and 
-#             td["SMA(9)"][i-1] < td["MACD"][i] and 
-#             td["MACD"][i] > 0 and i != 0):
-#         td.at[time, "Transaction"] = "buy"
-#     elif (td["SMA(9)"][i] < td["MACD"][i] and 
-#             td["SMA(9)"][i-1] > td["MACD"][i] and 
-#             td["MACD"][i] < 0 and i != 0):
-#         td.at[time, "Transaction"] = "sell"
-        
-# td = td.loc[td["Transaction"].isin(["buy", "sell"])]
-# td.to_excel("deals_report.xlsx")
+
+update_equity(balance, start_date)
+# Get tickers quotes with TF tf_entry
+df_entry = get_ticker(ticker, start_date, end_date, tf_entry)
+# Get ticker quotes with TF tf_exit
+df_exit = get_ticker(
+    ticker, start_date, end_date,
+    tf_exit, source="files", path=btcusd_30m_path)
+
+# Build MACD with TF tf_entry
+df_entry = build_macd(df_entry, tf_entry)
+# Build MACD with TF tf_exit
+df_exit = build_macd(df_exit, tf_exit)
+
+for i, _ in enumerate(df_entry.index):
+    # Check open condition #1
+    if open_cond1(df_entry, i):
+        # Move back from MACD < 0 point to find t1, t2
+        points = []
+        for k in range(i-1, -1, -1):
+            if k == i-1:
+                continue
+            if open_cond2(df_entry, k):
+                points.append(df_entry.index[k])
+                if len(points) == 2:
+                    points = list(map(lambda x: df_entry.loc[x], points))
+                    break
+        if len(points) < 2:
+            continue
+        t1, t2 = points
+        if open_cond3(t1, t2):
+            if open_cond4(t1, t2):
+                open_time = df_entry.index[i+1]
+                open_price = df_entry["Open"][i+1]
+                n = open_order("sell", str(to_datetime(open_time)), open_price)
+
+                for j, _ in enumerate(df_exit.index):
+                    if df_exit.index[j] > open_time:
+                        # Here we move back from the point with the time
+                        # that greater than open time
+                        points = []
+                        for k in range(j, i+1, -1):
+                            if k == j:
+                                continue
+                            if close_cond1(df_exit, k):
+                                points.append(df_exit.index[k])
+                                if len(points) == 2:
+                                   points = list(map(lambda x: df_exit.loc[x], points)) 
+                                   break
+                        if len(points) < 2:
+                            continue
+                        t1, t2 = points
+                        if close_cond2(t1, t2):
+                            if close_cond3(t1, t2):
+                                close_time = df_exit.index[j+1]
+                                close_price = df_exit["Open"][j+1]
+                                close_order(n, str(to_datetime(close_time)), close_price)
+                                break
+
+orders = pd.DataFrame(orders)
+pos_orders = orders[orders["net_income"] > 0]
+neg_orders = orders[orders["net_income"] < 0]
+# Average profit per positive order
+avg_pppo = reduce(lambda x,y: x+y, pos_orders["net_income"])/len(pos_orders.index)
+# Average loss per negative order
+avg_lpno = reduce(lambda x,y: x+y, neg_orders["net_income"])/len(neg_orders.index)
+# % of positive orders
+rel_po = len(pos_orders.index)/len(orders.index)*100
+# % of negative orders
+rel_no = len(neg_orders.index)/len(orders.index)*100
+annual_return = (balance - origin_balance)/(origin_balance * period) * 100 * 365
+print("Count trades: ", len(orders.index))
+print("Count pos trades: ", len(pos_orders.index))
+print("Count neg trades: ", len(neg_orders.index))
+print("Relative pos trades (%): ", rel_po)
+print("Relative neg trades (%): ", rel_no)
+print("Avg profit per pos trade:", avg_pppo)
+print("Avg loss per neg trade:", avg_lpno)
+print("Annual return (%): ", annual_return)
+writer = pd.ExcelWriter("orders2018_2020.xlsx", engine="xlsxwriter")
+orders.to_excel(writer, sheet_name="Orders 2018-2020")
+writer.save()
+equity = pd.DataFrame(equity["balance"], index=equity["datetime"], columns=["Equity"])
+equity.plot()
+plt.show()
